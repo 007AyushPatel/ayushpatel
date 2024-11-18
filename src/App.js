c960ab0752e3206b42167d71c2d0efcc95af8640
Patel <E110983@mcgladrey.rsm.net>
2024-11-18 19:55:39 +0530

    upadte

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+ import profilephotos from './images/profilephoto.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="leftPanel">
+          <img src={profilephotos} className="profilePhoto"/>
+       </div>
+       <div className="rightPanel">
+        <div><h1 style={{padding:0}}>Ayush Patel</h1></div>
+        <div><h4>IT Sector</h4></div>
+        <div><h4>ayu.ayushpatel@gmail.com</h4></div>
+        <div><h4>INDIA</h4></div>
+       </div>
     </div>
   );
 }
