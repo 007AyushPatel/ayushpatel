d142529260603f21e55a3113ab0cbf5b4fac84a6
Ayush Patel <77143521+007AyushPatel@users.noreply.github.com>
2024-11-19 10:50:25 +0000

    changes in app file

--- src/App.js
+++ src/App.js
@@ -24,19 +24,19 @@ function App() {
           offText="Off"
           onChange={_Change} 
         />
+      </div>
+      <div className="App">     
+        <div className="leftPanel">
+            <img src={profilephotos} className="profilePhoto"/>
+        </div>
+        <div className="rightPanel">
+          <div><h1 style={{padding:0}}> Ayush Patel</h1></div>
+          <div><h4>IT Sector</h4></div>
+          <div><h4>ayu.ayushpatel@gmail.com</h4></div>
+          <div><h4>INDIA</h4></div>
+        </div>
+      </div>
     </div>
-    <div className="App">     
-       <div className="leftPanel">
-          <img src={profilephotos} className="profilePhoto"/>
-       </div>
-       <div className="rightPanel">
-        <div><h1 style={{padding:0}}> Ayush Patel</h1></div>
-        <div><h4>IT Sector</h4></div>
-        <div><h4>ayu.ayushpatel@gmail.com</h4></div>
-        <div><h4>INDIA</h4></div>
-       </div>
-    </div>
-  </div>
   );
 }
 
